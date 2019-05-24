34bcea6bf8d38cb4e0d6ef8bc21d3a44ac6f0795
yogeshspawar2019 <50985698+yogeshspawar2019@users.noreply.github.com>
2019-05-24 16:04:26 +0530

    Update DALHelper.java

--- DALHelper.java
+++ DALHelper.java
@@ -1,5 +1,5 @@
 public class DALHelper {
 	Yogesh44445
 	private static final Logger logger = LoggerFactory.getLogger(DALHelper.class.getName());	
-	private static final Logger logger2 = LoggerFactory.getLogger(DALHelper.class.getName());
+	private static final Logger logger1 = LoggerFactory.getLogger(DALHelper.class.getName());
 }
