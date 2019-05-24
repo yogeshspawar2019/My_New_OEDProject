5c88a3e02c34be331d480a3bfa5180b569ef9523
yogeshspawar2019 <50985698+yogeshspawar2019@users.noreply.github.com>
2019-05-24 15:45:02 +0530

    Added new logger reference variable
    Added new logger reference variable

--- DALHelper.java
+++ DALHelper.java
@@ -1,4 +1,5 @@
 public class DALHelper {
 	Yogesh9087
-	private static final Logger logger = LoggerFactory.getLogger(DALHelper.class.getName());	
+	private static final Logger logger = LoggerFactory.getLogger(DALHelper.class.getName());
+	private static final Logger logger2 = LoggerFactory.getLogger(DALHelper.class.getName());
 }
