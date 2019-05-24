758469f44f25f284477528980dd2deedaf931451
yogeshspawar2019 <50985698+yogeshspawar2019@users.noreply.github.com>
2019-05-24 16:27:40 +0530

    Added new logger ref variable name as logger4
    Added new logger ref variable name as logger4

--- DALHelper.java
+++ DALHelper.java
@@ -3,4 +3,6 @@ public class DALHelper {
 	private static final Logger logger1 = LoggerFactory.getLogger(DALHelper.class.getName());	
 	private static final Logger logger2 = LoggerFactory.getLogger(DALHelper.class.getName());
 	private static final Logger logger3 = LoggerFactory.getLogger(DALHelper.class.getName());
+	private static final Logger logger4 = LoggerFactory.getLogger(DALHelper.class.getName());
+}
 }
